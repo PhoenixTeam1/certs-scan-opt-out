b06b4d01243f1d8b4d8be2cdc0a9efcebd21dd82
Mark O'Neill <mto@byu.edu>
2016-09-27 19:35:58 -0600

    Initial commit, barebones bootstrap with simple form

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Welcome</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <style>
+    body {
+        padding-top: 70px;
+        /* Required padding for .navbar-fixed-top. Remove if using .navbar-static-top. Change if height of navigation changes. */
+    }
+    </style>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">TLS Measurement Opt-Out</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="https://tlsresearch.byu.edu">Our Lab</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h1>Welcome! We hope we haven't disturbed you.</h1>
+	<p class="text-left">You likely stumbled upon this page because you are receiving abnormal internet traffic from this IP address.</p>
+	<p class="text-left">Our research lab is currently conducting measurements on the use of TLS certificates on the Internet. Currently we are performing scans on members of the Alexa top 1 million websites which support SSL/TLS on port 443. If you wish to opt out of being a part of the scan please submit your IP address of the server, or comma seperated list of them here.</p>
+	<form class="text-left" action="https://tlsresearch.byu.edu/optout.php" method="post">
+		<label for="ip">IP Address(es):</label><input name="ip" id="ip" value="<?php echo $_SERVER['REMOTE_ADDR'] ?>" type="text" />
+		<input type="submit" value="Opt Out" />
+	</form><br/>
+	<p class="text-left">If you have any questions please contact Mark O'Neill at mto@byu.edu. For more information on our lab, visit our <a href="http://tlsresearch.byu.edu/">site</a>.</p>
+            </div>
+        </div>
+        <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery Version 1.11.1 -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
